7844dc548936a533cd3434cc20a1693815e31205
guibafica <gb.engcomp@hotmail.com>
2020-10-22 22:11:36 -0300

    ADD: Configuracao de envio de imagem

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/', (req, res) => {
   return res.status(200).json({ message: 'Hello Happy!' });
 });
 
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 
 export default routes;
+
+// 1:18
